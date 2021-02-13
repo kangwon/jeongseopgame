1bfc597696b6cf13688e07bfcd7dd019699eb197
lee sangyeon <mpdark1227@gmail.com>
2021-02-13 18:31:19 +0900

    버튼 동작이 가능하게 스크립트 작성

---
+++ Assets/Scripts/EpisodeSelectPanelController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class EpisodeSelectPanelController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Button openPanelButton;
+    Button[] episodeButton = new Button[3];
+    GameObject episodeSelectPanel;
+    GameObject episodePanel;
+    void Start()
+    {
+        episodeSelectPanel = GameObject.Find("Canvas").transform.Find("EpisodeSelectPanel").gameObject;
+        episodePanel = GameObject.Find("Canvas").transform.Find("EpisodePanel").gameObject;
+        openPanelButton = GameObject.Find("Canvas/EpisodeSelectButton").GetComponent<Button>();
+        for (int i = 0; i < 3; i++)
+        {
+            episodeButton[i] = GameObject.Find($"Canvas/EpisodeSelectPanel/Episode{i + 1}Button").GetComponent<Button>();
+            episodeButton[i].onClick.AddListener(() => OnClickEpisodeButton());
+        }
+        episodeSelectPanel.SetActive(false);
+        openPanelButton.onClick.AddListener(() => OnClickEpisodeSelectButton());
+    }
+    void OnClickEpisodeSelectButton()
+    {
+        if (episodeSelectPanel.activeSelf == false)
+        {
+            Debug.Log(episodeSelectPanel.activeSelf);
+            episodeSelectPanel.SetActive(true);
+        }
+        else
+            episodeSelectPanel.SetActive(false);
+    }
+    void OnClickEpisodeButton()
+    {
+        episodePanel.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
