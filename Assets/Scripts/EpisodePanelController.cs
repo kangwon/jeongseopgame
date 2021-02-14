4f1c80969fd337bc6d9ae6d96bde8f89b6d84f70
kangwon <kang_won@naver.com>
2021-02-14 20:28:53 +0900

    Shorten code

--- Assets/Scripts/EpisodePanelController.cs
+++ Assets/Scripts/EpisodePanelController.cs
@@ -6,18 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class EpisodePanelController : MonoBehaviour
 {
-    GameObject episodePanel;
     Button okButton;
     Button cancelButton;
     Text episodeText;
-    // Start is called before the first frame update
+
     void Start()
     {
-        episodePanel = GameObject.Find("Canvas").transform.Find("EpisodePanel").gameObject;
         okButton = GameObject.Find("Canvas/EpisodePanel/OkButton").GetComponent<Button>();
-        okButton.onClick.AddListener(()=> OnClickOkButton());
+        okButton.onClick.AddListener(OnClickOkButton);
         cancelButton = GameObject.Find("Canvas/EpisodePanel/CancelButton").GetComponent<Button>();
-        cancelButton.onClick.AddListener(()=>OnClickCancelButton());
+        cancelButton.onClick.AddListener(OnClickCancelButton);
         episodeText = GameObject.Find("Canvas/EpisodePanel/EpisodeText").GetComponent<Text>();
     }
 
@@ -27,12 +25,6 @@ public class EpisodePanelController : MonoBehaviour
     }
     void OnClickCancelButton()
     {
-        episodePanel.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        this.gameObject.SetActive(false);
     }
 }
