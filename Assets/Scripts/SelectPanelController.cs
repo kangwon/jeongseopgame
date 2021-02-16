ca7b17d9eed9d4794d66ee475fc6e9acb52c0bea
kangwon <kang_won@naver.com>
2021-02-16 21:55:32 +0900

    Add SelectPanelController

---
+++ Assets/Scripts/SelectPanelController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectPanelController : MonoBehaviour
+{
+    GameObject ButtonPrefab;
+    GameObject ButtonHolder;
+
+    void Start()
+    {
+        ButtonPrefab = Resources.Load<GameObject>("AtionButtonPrefab");
+        ButtonHolder = GameObject.Find("ButtonHolder").gameObject;
+    }
+}
