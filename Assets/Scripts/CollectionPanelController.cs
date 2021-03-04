4f8312bc567ab4e7422ccf1d24fe39486a1e79ed
lee sangyeon <mpdark1227@gmail.com>
2021-03-04 17:51:15 +0900

    도감 패널생성 + 메인씬에서 도감버튼을 누르면 패널이 나오도록 설정 + 모든 에피소드에 따라 1개씩 버튼이 생성되게 설정

---
+++ Assets/Scripts/CollectionPanelController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectionPanelController : MonoBehaviour
+{
+    RectTransform rectTransform;
+    GameObject buttonPrefab;
+    GameObject buttonParent;
+    List<GameObject> buttonList = new List<GameObject> { };
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonPrefab = Resources.Load<GameObject>("CollectionButtonPrefab");
+        buttonParent = GameObject.Find("CollectionPanel/CollectionButtonListPanel/ScrollView/Viewport/Content");
+        rectTransform = this.gameObject.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = new Vector2(0, 0);
+        this.gameObject.SetActive(false);
+    }
+
+    public void AddAllEpisodeButtons()
+    {
+        foreach(var item in EpisodeCollection.Instance.AllEpisode)
+        {
+        GameObject button = Instantiate(buttonPrefab, buttonParent.transform);
+        buttonList.Add(button);
+        button.transform.GetComponentInChildren<Text>().text = item.title;
+        button.GetComponent<Button>().onClick.AddListener(() =>
+        {
+
+        });
+        }
+    }
+    void OnEnable()
+    {
+        if(buttonPrefab != null)
+        {
+            while (buttonList.Count != 0)
+            {
+                Destroy(buttonList[0]);
+                buttonList.RemoveAt(0);
+            }
+            AddAllEpisodeButtons();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/MainSceneUpdateController.cs
+++ Assets/Scripts/MainSceneUpdateController.cs
@@ -6,15 +6,20 @@ using UnityEngine.UI;
 public class MainSceneUpdateController : MonoBehaviour
 {
     Button openEpisodeSelectButton;
-    GameObject EpisodeSelectPanel;
+    Button collectionButton;
+    GameObject collectionPanel;
+    GameObject episodeSelectPanel;
     Text starText;
     // Start is called before the first frame update
     void Start()
     {
         openEpisodeSelectButton = GameObject.Find("Canvas/EpisodeSelectButton").GetComponent<Button>();
         openEpisodeSelectButton.onClick.AddListener(OnClickEpisodeSelectButton);
-        EpisodeSelectPanel = GameObject.Find("Canvas/EpisodeSelectPanel");
+        episodeSelectPanel = GameObject.Find("Canvas/EpisodeSelectPanel");
         starText = GameObject.Find("Canvas/UpperUI/Star/Text").GetComponent<Text>();
+        collectionPanel = GameObject.Find("Canvas/CollectionPanel");
+        collectionButton = GameObject.Find("Canvas/CollectionButton").GetComponent<Button>();
+        collectionButton.onClick.AddListener(() => collectionPanel.SetActive(true));
     }
 
     // Update is called once per frame
@@ -25,6 +30,6 @@ public class MainSceneUpdateController : MonoBehaviour
 
     void OnClickEpisodeSelectButton()
     {
-        EpisodeSelectPanel.SetActive(!EpisodeSelectPanel.activeSelf);
+        episodeSelectPanel.SetActive(!episodeSelectPanel.activeSelf);
     }
 }
