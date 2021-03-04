e16b3d6d1b1634d9467fd63277c3d8454c780e9c
lee sangyeon <mpdark1227@gmail.com>
2021-03-04 18:25:15 +0900

    뒤로가기버튼 이미지 추가

--- Assets/Scripts/CollectionPanelController.cs
+++ Assets/Scripts/CollectionPanelController.cs
@@ -8,30 +8,20 @@ public class CollectionPanelController : MonoBehaviour
     RectTransform rectTransform;
     GameObject buttonPrefab;
     GameObject buttonParent;
+    Button backButton;
     List<GameObject> buttonList = new List<GameObject> { };
     // Start is called before the first frame update
     void Start()
     {
         buttonPrefab = Resources.Load<GameObject>("CollectionButtonPrefab");
         buttonParent = GameObject.Find("CollectionPanel/CollectionButtonListPanel/ScrollView/Viewport/Content");
+        backButton = GameObject.Find("CollectionPanel/BackButton").GetComponent<Button>();
+        backButton.onClick.AddListener(BackButton);
         rectTransform = this.gameObject.GetComponent<RectTransform>();
-        rectTransform.anchoredPosition = new Vector2(0, 0);
+        rectTransform.anchoredPosition = new Vector2(0, -20);
         this.gameObject.SetActive(false);
     }
 
-    public void AddAllEpisodeButtons()
-    {
-        foreach(var item in EpisodeCollection.Instance.AllEpisode)
-        {
-        GameObject button = Instantiate(buttonPrefab, buttonParent.transform);
-        buttonList.Add(button);
-        button.transform.GetComponentInChildren<Text>().text = item.title;
-        button.GetComponent<Button>().onClick.AddListener(() =>
-        {
-
-        });
-        }
-    }
     void OnEnable()
     {
         if(buttonPrefab != null)
@@ -48,5 +38,22 @@ public class CollectionPanelController : MonoBehaviour
     void Update()
     {
         
+    }
+    public void AddAllEpisodeButtons()
+    {
+        foreach(var item in EpisodeCollection.Instance.AllEpisode)
+        {
+        GameObject button = Instantiate(buttonPrefab, buttonParent.transform);
+        buttonList.Add(button);
+        button.transform.GetComponentInChildren<Text>().text = item.title;
+        button.GetComponent<Button>().onClick.AddListener(() =>
+        {
+            button.GetComponent<RectTransform>().sizeDelta = new Vector2(0,100); // 누르면 버튼 사이즈 커지게
+        });
+        }
+    }
+    public void BackButton()
+    {
+        this.gameObject.SetActive(false);
     }
 }
