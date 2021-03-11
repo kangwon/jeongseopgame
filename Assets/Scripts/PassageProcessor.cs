54e6593a4f84ac094799638fdd0b598dbc1df49c
kangwon <kang_won@naver.com>
2021-03-11 19:57:44 +0900

    Evaluate expression in a set macro

--- Assets/Scripts/PassageProcessor.cs
+++ Assets/Scripts/PassageProcessor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
@@ -9,18 +10,18 @@ public class PassageProcessor
     public Passage passage = new Passage();
     public Dictionary<string, object> variables;
 
+    Regex stringRegex = new Regex(@"""(?<value>.+)""");
+    Regex intRegex = new Regex(@"(?<value>\d+)");
+    Regex floatRegex = new Regex(@"(?<value>[0-9.]+)");
+    Regex boolRegex = new Regex(@"(?<value>[false|true]+)");
+
     public PassageProcessor(Dictionary<string, object> initialVariables)
     {
         variables = initialVariables;
     }
 
     private object ParseType(string symbol)
-    {
-        Regex stringRegex = new Regex(@"""(?<value>.+)""");
-        Regex intRegex = new Regex(@"(?<value>\d+)");
-        Regex floatRegex = new Regex(@"(?<value>[0-9.]+)");
-        Regex boolRegex = new Regex(@"(?<value>[false|true]+)");
-        
+    {        
         Match stringMatch = stringRegex.Match(symbol);
         if(stringMatch.Success)
             return stringMatch.Groups["value"].Value;
@@ -40,14 +41,29 @@ public class PassageProcessor
         throw new NotImplementedException($"Invalid variable value: {symbol}");
     }
 
+    private object Eval(string expression)
+    {
+        if (stringRegex.IsMatch(expression))
+        {
+            return ParseType(expression);
+        }
+        else
+        {
+            var processedExpression = ProcessVariable(expression);
+            System.Data.DataTable table = new System.Data.DataTable();
+            var value = table.Compute(processedExpression, string.Empty);
+            return table.Compute(processedExpression, string.Empty);
+        }
+    }
+
     private string ProcessSetMacro(string originText)
     {
-        Regex rx = new Regex(@"\(set: \$(?<key>[a-zA-Z0-9_-]+) to (?<value>.+)\)");
+        Regex rx = new Regex(@"\(set:\s*\$(?<key>[a-zA-Z0-9_-]+) to (?<value>.+?)\)");
         MatchCollection matches = rx.Matches(originText);
         foreach (Match match in matches)
         {
             GroupCollection groups = match.Groups;
-            variables[groups["key"].Value] = ParseType(groups["value"].Value);
+            variables[groups["key"].Value] = Eval(groups["value"].Value);
         }
         // For debuging
         foreach (var kv in variables)
